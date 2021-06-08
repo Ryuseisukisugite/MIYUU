b2090bf14b0683b19ce717280ee538567a1f81bc
Ryuseisukisugite <2020m092@kuas.ac.jp>
2021-06-08 23:52:01 +0900

    グラフ１つ、円画像ができていません

--- app/src/main/java/com/example/miyuu/MainActivity.java
+++ app/src/main/java/com/example/miyuu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.miyuu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goTos312(View view) {
+        Intent tos312=new Intent(this, S312Activity.class);
+        startActivity(tos312);
     }
 }
---
+++ app/src/main/java/com/example/miyuu/S312Activity.java
@@ -0,0 +1,44 @@
+package com.example.miyuu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.razerdp.widget.animatedpieview.AnimatedPieView;
+import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig;
+import com.razerdp.widget.animatedpieview.data.SimplePieInfo;
+
+import java.util.ArrayList;
+
+import static android.graphics.Color.DKGRAY;
+
+public class S312Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s312);
+
+        drawPie();
+    }
+
+
+    public void drawPie()
+    {
+        AnimatedPieView mAnimatedPieView = findViewById(R.id.animatedPieView);
+        AnimatedPieViewConfig config = new AnimatedPieViewConfig();
+        config.startAngle(-90)// 起始角度偏移
+                .addData(new SimplePieInfo(30, Color.parseColor("#004E89"), "Title1"))
+                .addData(new SimplePieInfo(18.0f, Color.parseColor("#72ddf7"), "Title2")).drawText(true)
+                .duration(2000).textSize(60);// 持续时间
+
+// 以下两句可以直接用 mAnimatedPieView.start(config); 解决，功能一致
+        mAnimatedPieView.applyConfig(config);
+        mAnimatedPieView.start();
+    }
+}
